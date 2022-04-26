9ffa495c8455b4aad70e5ba252db8e0636088c12
N3pomuceno <jnfazevedo@hotmail.com>
2022-04-25 23:14:45 -0300

    Começo da parte 2

---
+++ part2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <threads.h>
+
+void *soma(){
+
+}
+
+int main(void) {
+    // Criação de matrizes A, B, C.
+
+    // Criação de 4 threads
+
+    // Desenvolvimento para a soma das duas matrizes.
+}
