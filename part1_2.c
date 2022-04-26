3588e92934103357ae0b851d02b1abb0d9a5fea8
N3pomuceno <jnfazevedo@hotmail.com>
2022-04-26 10:14:34 -0300

    Acrescentando explicação exercicio2 da parte1

--- part1_2.c
+++ part1_2.c
@@ -40,13 +40,28 @@ int main (void){
 // Links de referência:
 // https://stackoverflow.com/questions/19350212/which-linux-syscall-is-used-to-get-a-threads-id 
 // https://linux.die.net/man/2/gettid
+// https://www.delftstack.com/pt/howto/c/pthread-get-thread-id-in-c/ 
+// https://stackoverflow.com/questions/21091000/how-to-get-thread-id-of-a-pthread-in-linux-c-program 
 //
-// Comentário importante:
+// Comentários importante:
 // gettid() returns the caller's thread ID (TID). In a single-threaded process, the thread ID is equal
 // to the process ID (PID, as returned by getpid(2)).
 // In a multithreaded process, all threads have the same PID, but each one has a unique TID. 
+//
+// POSIX thread IDs are not the same as the thread IDs returned by the Linux specific gettid() system call.
+// POSIX thread IDs are assigned and maintained by the threading implementation.
+// The thread ID returned by gettid() is a number (similar to a process ID) that is assigned by the kernel.
+// Although each POSIX thread has a unique kernel thread ID in the Linux NPTL threading implementation,
+// an application generally doesn’t need to know about the kernel IDs (and won’t be portable if it depends on knowing them).
 // 
-// Explicação:
-// Basicamente quando utilizando o gettid do sys, nós pegamos um TID, que é uma thread única do processo.
-// Já quando pegando o thrd_current, estamos pegando o um outro valor, que seria um ponteiro que aponta para algum estrutura,
+// Extra:
+// Há 3 tipos de thread ID: pid, pthread id e tid;
+// - pid: Global e equivalente ao parent process id, pode ser pego utilizando getpid;
+// - pthread id (POSIX thread ID): Valor único do dentro do processo e somente na duração da devida thread. 
+// Não é único através do sistema, nem através das threads que já terminaram e começaram. NÃO é visível fora do programa.
+// Também é chamada de ID de Thread POSIX.
+//
+// Basicamente quando utilizando o gettid do sys, nós pegamos um PID, que é uma thread única do processo.
+// Já quando pegando o thrd_current, que está de acordo com os padrões do POSIX
+// ( Portable Operating System Interface for Unix) estamos pegando o um outro valor, que seria um ponteiro que aponta para algum estrutura,
 // dependendo da plataforma. 
