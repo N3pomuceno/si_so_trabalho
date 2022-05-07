46439ea17008b6a00aaadcce775818e7a9e67f67
N3pomuceno <jnfazevedo@hotmail.com>
2022-05-07 09:00:47 -0300

    Começando parte3, criação do vetor

--- part3.c
+++ part3.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <threads.h>
+#include <semaphore.h>
+#include <time.h>
 
+/* 
+Serão 3 casos para serem verificados:
+1 - Sem Threads;
+2 - Com Threads sem semáforos;
+3 - Com Threads com semáforos;
 
-void *remove () {
+*/
+
+void *remover () {
+
+}
+
+int correct(){
 
 }
 
 int main () {
+    // Criação de vetor.
+    int tam = 10;
+    int vet[tam];
+    for (int i = 0; i < tam; i++){
+        vet[i] = (rand() % 100) + 1;
+    }
+    //Checagem de vetor:
+    srand(time(0));
+    for (int i = 0; i < tam; i++){
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
 
     return 0;
 }
